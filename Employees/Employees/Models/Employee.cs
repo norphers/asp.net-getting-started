c82a9c8ac4c5fd0d7c086629c2eafbf6cbaaef3a
ocanades <ocanades@gmail.com>
2020-10-08 19:19:09 +0200

    exercise  employees => todo: frontend html-css-js & look for authentication tutorial

---
+++ Employees/Employees/Models/Employee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Employees.Models
+{
+    public class Employee
+    {
+        public long Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Post { get; set; }
+        public double Salary { get; set; }
+    }
+}
---
+++ Employees/Employees/Models/EmployeeDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Employees.Models
+{
+    public class EmployeeDTO
+    {
+        public long Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Post { get; set; }
+    }
+}
