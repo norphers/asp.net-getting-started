24fb66a7c60b62746fd4771ea3ec12b24475069d
ocanades <ocanades@gmail.com>
2020-10-27 17:25:13 +0100

    done => edit and delete functions todo => migrate DDBB, block ID edition, form validation.

--- Employees/Employees/wwwroot/js/site.js
+++ Employees/Employees/wwwroot/js/site.js
@@ -1,13 +1,11 @@
 const uri = '/api/Employees';
+
 let employees = [];
 
-function getEmployees() 
-{
-    fetch(uri)
-        .then(response => response.json())
-        .then(data => _displayEmployees(data))
-        .catch(error => console.error('Unable to get employees.', error));
-}
+//                                      //
+//            CRUD FUNCTIONS            //
+//                                      //
+
 
 function addEmployee() 
 {
@@ -42,6 +40,59 @@ function addEmployee()
         .catch(error => console.error('Unable to add employee.', error));
 }
 
+
+function getEmployees() 
+{
+    fetch(uri)
+        .then(response => response.json())
+        .then(data => _displayEmployees(data))
+        .catch(error => console.error('Unable to get employees.', error));
+}
+
+
+function updateEmployee() 
+{
+    const employeeId = document.getElementById('editId').value;
+
+    const employee = {
+        id: parseInt(employeeId, 10),
+        firstName: document.getElementById('editFirstName').value.trim(),
+        lastName: document.getElementById('editLastName').value.trim(),
+        position: document.getElementById('editPosition').value.trim()
+    };
+  
+    fetch(`${uri}/${employeeId}`, {
+      method: 'PUT',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(employee)
+    })
+    .then(() => getEmployees())
+    .catch(error => console.error('Unable to update employee.', error));
+  
+    closeInput();
+  
+    return false;
+}
+
+
+function deleteEmployee(id) 
+{
+    fetch(`${uri}/${id}`, {
+      method: 'DELETE'
+    })
+    .then(() => getEmployees())
+    .catch(error => console.error('Unable to delete employee.', error));
+}
+
+
+//                                      //
+//          DISPLAY FUNCTIONS           //
+//                                      //
+
+
 function _displayEmployees(data) 
 {
     const tEmployees = document.getElementById('employee-table');
@@ -74,13 +125,13 @@ function _displayEmployees(data)
     data.forEach(employee => {
         let editButton = button.cloneNode(false);
         editButton.innerText = 'Edit';
-        editButton.setAttribute("class", "btn btn-outline-secondary");
+        editButton.setAttribute('class', 'btn btn-outline-secondary');
         editButton.setAttribute('onclick', `displayEditForm(${employee.id})`);
     
         let deleteButton = button.cloneNode(false);
         deleteButton.innerText = 'Delete';
-        deleteButton.setAttribute("class", "btn btn-outline-danger");
-        deleteButton.setAttribute('onclick', `deleteItem(${employee.id})`);
+        deleteButton.setAttribute('class', 'btn btn-outline-danger');
+        deleteButton.setAttribute('onclick', `deleteEmployee(${employee.id})`);
         
         let tr2 = tEmployees.insertRow();
 
@@ -106,151 +157,26 @@ function _displayEmployees(data)
     employees = data;
 }
 
-function deleteEmployee(id) 
+
+function displayEditForm(id) 
 {
-    fetch(`${uri}/${id}`, {
-      method: 'DELETE'
-    })
-    .then(() => getEmployees())
-    .catch(error => console.error('Unable to delete employee.', error));
+    const employee = employees.find(employee => employee.id === id);
+
+    document.getElementById('editId').value = employee.id;
+    document.getElementById('editFirstName').value = employee.firstName;
+    document.getElementById('editLastName').value = employee.lastName;
+    document.getElementById('editPosition').value = employee.position;
+
+    document.getElementById('edit-employee-container').style.display = 'block';
+    document.getElementById('employee-container').style.display = 'none';
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-const uri = 'api/TodoItems';
-let todos = [];
-
-function getItems() {
-  fetch(uri)
-    .then(response => response.json())
-    .then(data => _displayItems(data))
-    .catch(error => console.error('Unable to get items.', error));
-}
-
-function addItem() {
-  const addNameTextbox = document.getElementById('add-name');
-
-  const item = {
-    isComplete: false,
-    name: addNameTextbox.value.trim()
-  };
-
-  fetch(uri, {
-    method: 'POST',
-    headers: {
-      'Accept': 'application/json',
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify(item)
-  })
-    .then(response => response.json())
-    .then(() => {
-      getItems();
-      addNameTextbox.value = '';
-    })
-    .catch(error => console.error('Unable to add item.', error));
-}
-
-function deleteItem(id) {
-  fetch(`${uri}/${id}`, {
-    method: 'DELETE'
-  })
-  .then(() => getItems())
-  .catch(error => console.error('Unable to delete item.', error));
-}
-
-function displayEditForm(id) {
-  const item = todos.find(item => item.id === id);
   
-  document.getElementById('edit-name').value = item.name;
-  document.getElementById('edit-id').value = item.id;
-  document.getElementById('edit-isComplete').checked = item.isComplete;
-  document.getElementById('editForm').style.display = 'block';
+ 
+function closeInput() 
+{
+    document.getElementById('edit-employee-container').style.display = 'none';
+    document.getElementById('employee-container').style.display = 'block';
 }
 
-function updateItem() {
-  const itemId = document.getElementById('edit-id').value;
-  const item = {
-    id: parseInt(itemId, 10),
-    isComplete: document.getElementById('edit-isComplete').checked,
-    name: document.getElementById('edit-name').value.trim()
-  };
 
-  fetch(`${uri}/${itemId}`, {
-    method: 'PUT',
-    headers: {
-      'Accept': 'application/json',
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify(item)
-  })
-  .then(() => getItems())
-  .catch(error => console.error('Unable to update item.', error));
-
-  closeInput();
-
-  return false;
-}
-
-function closeInput() {
-  document.getElementById('editForm').style.display = 'none';
-}
-
-function _displayCount(itemCount) {
-  const name = (itemCount === 1) ? 'to-do' : 'to-dos';
-
-  document.getElementById('counter').innerText = `${itemCount} ${name}`;
-}
-
-function _displayItems(data) {
-  const tBody = document.getElementById('todos');
-  tBody.innerHTML = '';
-
-  _displayCount(data.length);
-
-  const button = document.createElement('button');
-
-  data.forEach(item => {
-    let isCompleteCheckbox = document.createElement('input');
-    isCompleteCheckbox.type = 'checkbox';
-    isCompleteCheckbox.disabled = true;
-    isCompleteCheckbox.checked = item.isComplete;
-
-    let editButton = button.cloneNode(false);
-    editButton.innerText = 'Edit';
-    editButton.setAttribute('onclick', `displayEditForm(${item.id})`);
-
-    let deleteButton = button.cloneNode(false);
-    deleteButton.innerText = 'Delete';
-    deleteButton.setAttribute('onclick', `deleteItem(${item.id})`);
-
-    let tr = tBody.insertRow();
-    
-    let td1 = tr.insertCell(0);
-    td1.appendChild(isCompleteCheckbox);
-
-    let td2 = tr.insertCell(1);
-    let textNode = document.createTextNode(item.name);
-    td2.appendChild(textNode);
-
-    let td3 = tr.insertCell(2);
-    td3.appendChild(editButton);
-
-    let td4 = tr.insertCell(3);
-    td4.appendChild(deleteButton);
-  });
-
-  todos = data;
-}
-*/
+document.getElementById('edit-employee-container').style.display = 'none';
